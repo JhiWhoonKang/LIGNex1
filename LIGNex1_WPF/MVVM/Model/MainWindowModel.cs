e5090b43a617c64d70a006fe3f0d4427a5778efd
JhiWhoon Kang <109329462+JhiWhoonKang@users.noreply.github.com>
2025-02-07 17:50:19 +0900

    LIGNex1 C# WPF
    LIGNex1 C# WPF

---
+++ LIGNex1_WPF/MVVM/Model/MainWindowModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MVVM.Model
+{
+    class MainWindowModel : INotifyPropertyChanged
+    {
+        private int num = 1;
+
+        public int Num
+        {
+            get { return num; }
+            set { num = value; onPropertyChanged("Num"); }
+        }
+
+        private string name = "aaa";
+        public string Name
+        {
+            get { return name; }
+            set { name = value; onPropertyChanged("Name"); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void onPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+        }
+    }
+}
---
+++ LIGNex1_WPF/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM.Model;
+
+namespace MVVM.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private MainWindowModel model;
+        public MainWindowModel Model
+        {
+            get { return model; }
+            set { model = value; onPropertyChanged("Model"); }
+        }
+        public MainWindowViewModel()
+        {
+            model = new MainWindowModel();
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void onPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            { 
+                handler(this, new PropertyChangedEventArgs(propertyName)); 
+            }
+        }
+    }
+}
